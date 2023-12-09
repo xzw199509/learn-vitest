f87d161a1d17277b47d358a7bfa8eeebed62ad11
xiao1995 <1050149954@qq.com>
2023-12-09 18:24:52 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-red-500">{{map}}</div>
+</template>
+<script setup lang="ts">
+import { useMapStore } from './store/map'
+
+const { map } = useMapStore()
+</script>
+<style scoped></style>
